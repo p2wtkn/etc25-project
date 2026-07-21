9de2e10e6fabae2c1884d4c02d0a840435a7590d
p2wtkn <ps.p2wtkn@gmail.com>
2026-07-21 23:23:24 +0700

    typecasting

--- test_python/main.py
+++ test_python/main.py
@@ -2,35 +2,35 @@
 #            A variable behave as if it was the value it contains
 
 # Strings
-first_name = "p2wtkn"
-drink = "coffee"
-email = "p2wtkn@fake.com"
+# first_name = "p2wtkn"
+# drink = "coffee"
+# email = "p2wtkn@fake.com"
 
 # print(f"I'm {first_name}")
 # print(f"I like {drink}")
 # print(f"Yours email is {email}")
 
 # Integers
-age = 22
-quantity = 3
-num_of_students = 28
+# age = 22
+# quantity = 3
+# num_of_students = 28
 
 # print(f"You're {age} years old")
 # print(f"You're buying {quantity} items")
 # print(f"Your class has {num_of_students} students")
 
 # Float
-price = 10.25
-gpa = 3.5
-distance = 5.5
+# price = 10.25
+# gpa = 3.5
+# distance = 5.5
 
 # print(f"The price is {price}฿")
 # print(f"My gpa is: {gpa}")
 # print(f"I ran {distance}km.")
 
 # Booleans
-is_students = True
-for_sale = False
+# is_students = True
+# for_sale = False
 
 # print(f"Are you a student?: {is_students}")
 # if is_students:
@@ -43,3 +43,29 @@ for_sale = False
 #     print("Yes, it is.")
 # else:
 #     print("No, it isn't.")
+
+
+
+# Typecasting = the process of converting a variable from one data type to another
+#               str(), int(), float(), bool()
+
+# name = "p2wtkn"
+# name2 = ""
+# age = 22
+# gpa = 3.2
+# is_students = True
+
+# print(f"name = {name}", type(name))
+# age = str(age)
+# print(f"age = {age}", type(age))
+# # age += 1
+# # age += "1"
+# # print(age)
+# age = float(age)
+# print(f"age = {age}", type(age))
+# gpa = int(gpa)
+# print(f"gpa = {gpa}", type(gpa))
+# name = bool(name)
+# print(f"name = {name}", type(name))
+# name2 = bool(name2)
+# print(f"name2 = {name2}", type(name2))
