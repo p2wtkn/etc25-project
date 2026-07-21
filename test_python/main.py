d87591d4464a22587f22344cb73441344505778f
p2wtkn <ps.p2wtkn@gmail.com>
2026-07-21 22:13:36 +0700

    variable

---
+++ test_python/main.py
@@ -0,0 +1,45 @@
+# Variable = A container for a value (string, integer, float, boolean)
+#            A variable behave as if it was the value it contains
+
+# Strings
+first_name = "p2wtkn"
+drink = "coffee"
+email = "p2wtkn@fake.com"
+
+# print(f"I'm {first_name}")
+# print(f"I like {drink}")
+# print(f"Yours email is {email}")
+
+# Integers
+age = 22
+quantity = 3
+num_of_students = 28
+
+# print(f"You're {age} years old")
+# print(f"You're buying {quantity} items")
+# print(f"Your class has {num_of_students} students")
+
+# Float
+price = 10.25
+gpa = 3.5
+distance = 5.5
+
+# print(f"The price is {price}฿")
+# print(f"My gpa is: {gpa}")
+# print(f"I ran {distance}km.")
+
+# Booleans
+is_students = True
+for_sale = False
+
+# print(f"Are you a student?: {is_students}")
+# if is_students:
+#     print("You are a student.")
+# else:
+#     print("You are NOT a students.")
+
+# print(f"Is it for sale?: {for_sale}")
+# if for_sale:
+#     print("Yes, it is.")
+# else:
+#     print("No, it isn't.")
