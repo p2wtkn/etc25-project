aba2d79a4afc9b9552ad5f3f2be3f2c75752f541
p2wtkn <ps.p2wtkn@gmail.com>
2026-07-22 19:11:58 +0700

    Madlibs game

--- test_python/main.py
+++ test_python/main.py
@@ -102,3 +102,20 @@
 
 # print(f"You have bought {quantity} x {item}/s")
 # print(f"Your total is: {total}฿")
+
+
+
+# Madlibs game
+# word game where you create a story
+# by filling in blanks with random words
+
+# adjective1 = input("Enter an adjective (description): ")
+# noun1 = input("Enter a noun (person, place, thing): ")
+# adjective2 = input("Enter an adjective (description): ")
+# verb1 = input("Enter a verb ending with 'ing': ")
+# adjective3 = input("Enter an adjective (description): ")
+
+# print(f"Today I went to a {adjective1} zoo.")
+# print(f"In an exhibit, I saw a {noun1}")
+# print(f"{noun1} was {adjective2} and {verb1}")
+# print(f"I was {adjective3}!")
